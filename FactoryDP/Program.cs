e4f8a47b176e1c7a91a6999281defc42a8ef021f
kasireddyC <35594822+kasireddyC@users.noreply.github.com>
2022-11-20 20:25:05 +0530

    Add files via upload

--- FactoryDP/Program.cs
+++ FactoryDP/Program.cs
@@ -1,46 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FactoryDP
 {
     public class Program
     {
+        public static void PrintOutputDetails(ICreditCard creditCard)
+        {
+            Console.WriteLine("Card Type : " + creditCard.GetCardType());
+            Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+            Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            Console.WriteLine("----------------------------");
+        }
         static void Main(string[] args)
         {
             CreditCardFactory creditCardFactory = new ConcreteCreditCardFactory();
             ICreditCard platinumCard = creditCardFactory.GetCard("Platinum");
             if (platinumCard != null)
             {
-                Console.WriteLine("Card Type : " + platinumCard.GetCardType());
-                Console.WriteLine("Credit Limit : " + platinumCard.GetCreditLimit());
-                Console.WriteLine("Annual Charge :" + platinumCard.GetAnnualCharge());
+                PrintOutputDetails(platinumCard);
             }
             else
             {
                 Console.Write("Invalid Card Type");
             }
-            Console.WriteLine("--------------");
             ICreditCard moneyBackCard = creditCardFactory.GetCard("MoneyBack");
             if (moneyBackCard != null)
             {
-                Console.WriteLine("Card Type : " + moneyBackCard.GetCardType());
-                Console.WriteLine("Credit Limit : " + moneyBackCard.GetCreditLimit());
-                Console.WriteLine("Annual Charge :" + moneyBackCard.GetAnnualCharge());
+                PrintOutputDetails(moneyBackCard);
             }
             else
             {
                 Console.Write("Invalid Card Type");
             }
-            Console.WriteLine("--------------");
             ICreditCard titaniumCard = creditCardFactory.GetCard("Titanium");
             if (titaniumCard != null)
             {
-                Console.WriteLine("Card Type : " + titaniumCard.GetCardType());
-                Console.WriteLine("Credit Limit : " + titaniumCard.GetCreditLimit());
-                Console.WriteLine("Annual Charge :" + titaniumCard.GetAnnualCharge());
+                PrintOutputDetails(titaniumCard);
             }
             else
             {
